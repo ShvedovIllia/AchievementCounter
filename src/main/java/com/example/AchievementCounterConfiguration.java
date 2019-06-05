fee754a67a5ea275582767aa5e155a2c430d9875
ShvedovIllia <shvedov.illia@gmail.com>
2019-06-05 12:21:49 +0300

    add datasource bean

--- src/main/java/com/example/AchievementCounterConfiguration.java
+++ src/main/java/com/example/AchievementCounterConfiguration.java
@@ -1,18 +1,28 @@
 package com.example;
 
-import com.example.DBConnection.DataSource;
+//import com.example.DBConnection.DataSource;
+
+import com.example.DBConnection.DataSourceBean;
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AchievementCounterConfiguration {
 
+    private final DataSourceBean dataSource;
+
+    @Autowired
+    public AchievementCounterConfiguration(DataSourceBean dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public SpringLiquibase liquibase() {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setChangeLog("classpath:db/changelog/db-changes-master.yaml");
-        liquibase.setDataSource(DataSource.getDataSource());
+        liquibase.setDataSource(dataSource.getDataSource());
         return liquibase;
     }
 }
---
+++ src/main/java/com/example/DBConnection/DataSourceBean.java
@@ -0,0 +1,39 @@
+package com.example.DBConnection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Configuration
+@Component
+public class DataSourceBean {
+    private static HikariDataSource ds;
+    private HikariConfig config = new HikariConfig();
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    @Bean
+    @Primary
+    public HikariDataSource getDataSource() {
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/achievement_counter?serverTimezone=UTC");
+        config.setUsername("root");
+        config.setPassword("1234");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+        return ds;
+    }
+}
