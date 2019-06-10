4b5fe53c7b31bc059d3a960b345a38a5c6199bc7
ShvedovIllia <shvedov.illia@gmail.com>
2019-06-10 18:19:29 +0300

    add logs

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -25,21 +25,31 @@ public class UserController {
 
     @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserEntity> getUserById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(userService.getEntityById(id));
+        logger.info("Request to get user with id = " + id);
+        UserEntity userEntity = userService.getEntityById(id);
+        logger.info("User with id " + id + "! \nName: " + userEntity.getName() + "\nPassword: " + userEntity.getPassword() +
+                "\nDate of create: " + userEntity.getDateOfCreation() + "\nTeam id: " + userEntity.getTeamId());
+        return ResponseEntity.ok(userEntity);
     }
 
     @RequestMapping(value = "/users/getAll", method = RequestMethod.GET)
     public ResponseEntity<List<UserEntity>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
+        logger.info("Request to get all users");
+        List<UserEntity> users = userService.getAllUsers();
+        logger.info(users.size() + " users found!");
+        return ResponseEntity.ok(users);
     }
 
     @RequestMapping(value = "/users/create", method = RequestMethod.POST)
     public ResponseEntity<?> createUser(@RequestBody UserDTO userDTO) {
+        logger.info("Request model:\nName: " + userDTO.getName() + "\nPassword: " + userDTO.getPassword() + "\nDate of create: "
+                + userDTO.getDateOfCreation() + "\nTeam id: " + userDTO.getTeamId());
         return ResponseEntity.ok(userService.createUser(userDTO));
     }
 
     @RequestMapping(value = "/users/update/{id}", method = RequestMethod.PUT)
     public ResponseEntity<?> updateUser(@RequestBody UserDTO userDTO, @PathVariable("id") Long id) {
+        logger.info("Request to update user with id = " + id);
         return ResponseEntity.ok(userService.updateUser(userDTO, id));
     }
 }
