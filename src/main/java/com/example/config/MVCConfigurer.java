5d8e83d71a8c8ecb0d32616ea6bea753aa802d70
ShvedovIllia <shvedov.illia@gmail.com>
2019-06-11 17:09:34 +0300

    add auto logs for all controllers

--- src/main/java/com/example/config/MVCConfigurer.java
+++ src/main/java/com/example/config/MVCConfigurer.java
@@ -7,18 +7,22 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class UsersMVCConfigurer implements WebMvcConfigurer {
+public class MVCConfigurer implements WebMvcConfigurer {
 
     private final UsersRequestIncreptor usersRequestIncreptor;
 
     @Autowired
-    public UsersMVCConfigurer(UsersRequestIncreptor usersRequestIncreptor) {
+    public MVCConfigurer(UsersRequestIncreptor usersRequestIncreptor) {
         this.usersRequestIncreptor = usersRequestIncreptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(usersRequestIncreptor)
-                .addPathPatterns("/**/users/**/");
+                .addPathPatterns("/**/users/**/")
+                .addPathPatterns("/**/teams/**/")
+                .addPathPatterns("/**/achievements/**/")
+                .addPathPatterns("/**/activity/**/");
+
     }
 }
