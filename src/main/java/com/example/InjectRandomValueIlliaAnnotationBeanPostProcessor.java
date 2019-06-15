586c5435733555090a7872baff0ee3d54630402c
ShvedovIllia <shvedov.illia@gmail.com>
2019-06-15 18:12:42 +0300

    add bean configs

---
+++ src/main/java/com/example/InjectRandomValueIlliaAnnotationBeanPostProcessor.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import com.example.entity.user.RandomValueIllia;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+public class InjectRandomValueIlliaAnnotationBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            RandomValueIllia annotation = field.getAnnotation(RandomValueIllia.class);
+            if (annotation != null) {
+                int min = annotation.min();
+                int max = annotation.max();
+                Random random = new Random();
+                int i = random.nextInt(max - min);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, bean, i);
+            }
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
--- src/main/java/com/example/entity/user/UserEntity.java
+++ src/main/java/com/example/entity/user/UserEntity.java
@@ -2,15 +2,14 @@ package com.example.entity.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 
 public class UserEntity {
@@ -21,4 +20,23 @@ public class UserEntity {
     private String password;
     private LocalDate dateOfCreation;
     private Long teamId;
+
+    private String testField;
+    @RandomValueIllia(min = 2, max = 9)
+    private int someRandom;
+
+    public UserEntity() {
+        System.out.println("Constructor");
+    }
+
+    public void testMethod() {
+        for (int i = 0; i < someRandom; i++) {
+            System.out.println(testField);
+        }
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("init method");
+    }
 }
